9201c1fdb771d2cbae221320f8e7276509e85c40
samsonajoshua@gmail.com <samsonajoshua@gmail.com>
2025-07-17 10:04:19 +0800

    Added hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { HashRouter } from 'react-router-dom';
 import ProductsDropdown from './components/machinesDropDown';
 function App() {
   return (
     <div style={{ padding: '60px'}}>
-      <ProductsDropdown />
+      <HashRouter>
+        <ProductsDropdown />
+      </HashRouter>
+
     </div>
 
   );
