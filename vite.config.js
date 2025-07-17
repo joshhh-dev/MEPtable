3ee1f5ce85664f9c0c67f61642b177929e07f8cd
samsonajoshua@gmail.com <samsonajoshua@gmail.com>
2025-07-17 09:46:44 +0800

    Add github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'joshhh-dev.github.io/MEPtable',
+  base: 'joshhh-dev.github.io/MEPtable/',
   plugins: [react()],
 })
