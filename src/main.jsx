01880fdef8da531a8a024ca0f5adf135b8af899a
samsonajoshua@gmail.com <samsonajoshua@gmail.com>
2025-07-17 10:07:36 +0800

    dito ang hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import React from 'react'
 
 createRoot(document.getElementById('root')).render(
-  //<React.StrictMode>
+  <HashRouter>
+
     <App />
-  //</React.StrictMode>,
+
+  </HashRouter>
+
 )
